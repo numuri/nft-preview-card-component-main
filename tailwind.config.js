9d811cfe5f0fb1f41b7f348f35efb59b439f93b3
Nur Muhammad Rizky <nrmrizky5@gmail.com>
2023-07-06 10:32:56 +0700

    make nft preview card with frontend mentor

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      colors: {
+        softBlue: "hsl(215, 51%, 70%)",
+        cyan: "hsl(178, 100%, 50%)",
+        bgVeryDarkBlue: "hsl(217, 54%, 11%)",
+        cardVeryDarkBlue: "hsl(216, 50%, 16%)",
+        lineveryDarkBlue: "hsl(215, 32%, 27%)",
+        white: "hsl(0, 0%, 100%)",
+      },
+    },
+  },
+  plugins: [],
+};
